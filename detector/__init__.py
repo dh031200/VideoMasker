19a6cd67eaea36da76906e8c3aa143fb289a3518
Danny Kim <imbird0312@gmail.com>
2024-06-06 23:40:10 +0900

    Cuda available (#6)
    * Add requirements.txt
    
    Added a new file, requirements.txt, that lists all the dependencies required for the project. This file includes packages like pyside6, torch, torchvision among others. With these requirements in place, it is easier for other developers to set up the project in their local environment.
    
    * Update .gitignore to include common Python files
    
    The .gitignore file has been extensively updated with rules to exclude common files that often need to be ignored in Python projects. This includes compiled Python files, pip logs, unit test coverage reports, Sphinx documentation build files, Jupyter notebook checkpoints, Pycharm, among others. This will prevent unnecessary files from being tracked by Git.
    
    * Update tracking method and clean up unused code
    
    The SFSORT tracking method was commented out and the BYTETracker falls back as the default method. Also, the code related to the EmbedDetector class was removed due to remove unused portion, and numpy with pillow were added to the requirements. The condition for using cuda has been slightly updated as well.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- detector/__init__.py
+++ detector/__init__.py
@@ -1,8 +1,6 @@
 # SPDX-FileCopyrightText: 2024-present dh031200 <imbird0312@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-from collections import defaultdict
-
 import cv2
 import torch
 import numpy as np
@@ -10,90 +8,10 @@ import supervision as sv
 from ultralytics import YOLO
 from imgbeddings import imgbeddings
 from PIL import Image
-from sklearn.metrics.pairwise import cosine_similarity
 
 from .tracker import Tracker
 from .cropper import Cropper
 from .utils import to_supervision_detections, make_labels
-
-
-class EmbedDetector:
-    def __init__(self):
-        model_path = "yolov8s-oiv7-face.pt"
-        device = "mps"
-        self.model = YOLO(model_path, task="detect")
-        self.device = device
-
-        self.box_annotator = sv.BoundingBoxAnnotator(thickness=2)
-        self.label_annotator = sv.LabelAnnotator(text_scale=0.5, text_padding=2)
-        self.embedder = imgbeddings()
-
-        self.face_embeddings_dict = defaultdict(int)
-        self.face_embeddings_group = defaultdict(list)
-        self.face_embedding_list = []
-        self.face_image_list = []
-        self.box_annotator = sv.BoundingBoxAnnotator(thickness=2)
-        self.label_annotator = sv.LabelAnnotator(text_scale=0.5, text_padding=2)
-        self.face_idx = 0
-        self.global_idx = 0
-
-    def __call__(self, frame):
-        detections = self.detect(frame)
-        detections = self.track(frame, detections)
-        return detections
-
-    def detect(self, frame):
-        det = self.model(frame, stream=True, verbose=False, device=self.device)
-        return next(det).boxes.data.cpu().numpy()
-
-    def get_embedding(self, img):
-        img_cropped = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img_embedding = self.embedder.to_embeddings(Image.fromarray(img_cropped))
-        return img_embedding[0]
-
-    def track(self, frame, detections):
-        dets = []
-        for detection in detections:
-            left, top, right, bottom = map(int, detection[:4])
-            cropped_img = frame[top:bottom, left:right]
-            embedding = self.get_embedding(cropped_img)
-            max_similarity = 0
-            matched_face_idx = self.face_idx
-            if len(self.face_embedding_list):
-                similarity = cosine_similarity(self.face_embedding_list, [embedding])
-                max_similarity_idx = similarity.argmax()
-                max_similarity = similarity[max_similarity_idx][0]
-                if max_similarity > 0.9:
-                    matched_face_idx = self.face_embeddings_dict[max_similarity_idx]
-                    self.face_embeddings_group[matched_face_idx].append(self.global_idx)
-                else:
-                    self.face_idx += 1
-
-            else:
-                self.face_idx += 1
-            self.face_embeddings_dict[self.global_idx] = matched_face_idx
-            self.face_embeddings_group[self.face_idx].append(self.global_idx)
-            self.face_embedding_list.append(embedding)
-            self.global_idx += 1
-            dets.append([left, top, right, bottom, max_similarity, 0, matched_face_idx])
-        return np.array(dets)
-
-    def visualize(self, original_image, detections):
-        if len(detections):
-            detections = to_supervision_detections(detections)
-
-            annotated_frame = self.box_annotator.annotate(
-                scene=original_image,
-                detections=detections,
-            )
-            annotated_frame = self.label_annotator.annotate(
-                scene=annotated_frame,
-                detections=detections,
-                labels=make_labels(detections),
-            )
-        else:
-            annotated_frame = original_image
-        return annotated_frame
 
 
 class Detector:
@@ -101,7 +19,7 @@ class Detector:
         if torch.backends.mps.is_available() and False:
             model_path = "model/yolov8s-oiv7-face.mlpackage"
             device = "mps"
-        elif torch.cuda.is_available() and False:
+        elif torch.cuda.is_available():
             model_path = "model/yolov8s-oiv7-face.pt"
             device = "cuda"
         else:
@@ -110,7 +28,7 @@ class Detector:
         self.model = YOLO(model_path, task="detect")
         self.device = device
         self.tracker = Tracker(method="bytetrack")
-        # self.tracker = Tracker(method="SFSORT", fps=fps, width=width, height=height)
+        # self.tracker = Tracker(method="SFSORT", fps=fps, width=width, height=height) # TODO
         self.cropper = Cropper(capture_thresh=fps // 2, save_dir="cropped_images")
         self.box_annotator = sv.BoundingBoxAnnotator(thickness=2)
         self.label_annotator = sv.LabelAnnotator(text_scale=0.5, text_padding=2)
@@ -148,3 +66,6 @@ class Detector:
             scene=annotated_frame, detections=detections, labels=make_labels(detections)
         )
         return annotated_frame
+
+
+__all__ = ("Detector",)
